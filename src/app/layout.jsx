d3ceb51d96240efaf7e3ee667b3687e46ee05a92
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2024-02-19 23:10:12 +0530

    changes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -14,9 +14,17 @@ export const metadata = {
     template: '%s | Unchiudaan Classes ',
   },
   description: "Current Affairs for UPSC, BPSC, बिहार दारोगा, SI, BSSC, Railway, JSSC, SSC, BANKING, Defence, और अन्य Government Job Examinations के लिए ऊँची उड़ान वेबसाइट को join करें।",
+  keywords: ['Daily Quiz / डेली प्रश्न', 'Daily Current Affairs/ कर्रेंट अफेयर्स', 'Monthly PDFs / मासिक PDF ', 'News / Blog ,unchi udan classes', 'unchiudaanclasses', 'uchiudaan classes','uchiudan','Unchiudaan classes','ऊँची उड़ान classes',
+  'Daily Current Affairs','Unchiudaan Current Affairs', 'Current Affairs for UPSC', 'BPSC','बिहार दारोगा','SI','BSSC','Railway','JSSC', 'SSC', 'BANKING', 'Defence','और अन्य Government Job Examinations'],
   twitter: {
     card: 'summary_large_image',
-  }
+  },
+  openGraph: {
+    images: 'https://unchiudaanclasses.com/uchiudan.png',
+    width: 800,
+    height: 600,
+  },
+  // ogImage:'https://unchiudaanclasses.com/uchiudan.png'
 };
 
 export default function RootLayout({ children }) {
