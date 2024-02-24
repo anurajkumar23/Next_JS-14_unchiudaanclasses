82e0cd1264e92b72786cca6392f706bf99990fdd
Ishu Singh <104965112+ishuoncode@users.noreply.github.com>
2024-02-24 23:38:49 +0530

    fix issues of dtudymaterials

--- src/app/studymaterials/page.jsx
+++ src/app/studymaterials/page.jsx
@@ -6,7 +6,7 @@ import { useGetUserQuery } from '../redux/slices/userSlices';
 
 export default function page() {
     const { data: userData} = useGetUserQuery();
-    console.log("🚀 ~ page ~ userData:", userData)
+
   return (
     <div>
       <Studymaterialpage userData={userData}/>
