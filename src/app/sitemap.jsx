f5a010550cef5a21f3cd2b7a600ac7f90f68d93f
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2024-02-25 12:37:53 +0530

    sitemap changes

--- src/app/sitemap.jsx
+++ src/app/sitemap.jsx
@@ -4,7 +4,9 @@ export default async function sitemap(){
     const  baseurl = `${process.env.NEXT_PUBLIC_FRONTEND_URL}`
 
 
-    const response1 = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/currentaffairs`) 
+    const response1 = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/currentaffairs`,
+    { cache: 'no-store' }
+    ) 
     const data1 = await response1.json()
     const affairs = data1.data.affairs;
     const affairsUrl = affairs.map((affairs) => ({
@@ -13,7 +15,9 @@ export default async function sitemap(){
     }));
 
 
-    const response2 = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/News`) 
+    const response2 = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/News`,
+    { cache: 'no-store' }
+    ) 
     const data2 = await response2.json()
     const news = data2.data.news;
     const newsUrl = news.map((news) => ({
@@ -21,7 +25,9 @@ export default async function sitemap(){
         lastModified: news.updatedAt,
     }));
 
-    const response3 = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/pdfs`) 
+    const response3 = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/pdfs`,
+    { cache: 'no-store' }
+    ) 
     const data3 = await response3.json()
     const pdf = data3.data.pdf;
     const pdfUrl = pdf.map((pdf) => ({
@@ -29,7 +35,9 @@ export default async function sitemap(){
         lastModified: pdf.updatedAt,
     }));
 
-    const response4 = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/test`) 
+    const response4 = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/test`,
+    { cache: 'no-store' }
+    ) 
     const data4 = await response4.json()
     const tests = data4.data.tests;
     const testsUrl = tests.map((tests) => ({
